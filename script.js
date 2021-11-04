9341c978f1be6c75fe2e8fb78f6485307c2be6c7
sadyng <iuyals@qq.com>
2021-11-04 20:43:29 +0800

    my mvc catClicker

--- script.js
+++ script.js
@@ -1,42 +1,67 @@
-let catContainer = document.querySelector('#catContainer');
+let model = {
+  acat: {
+    name: 'acat',
+    catClickCounter: 0,
+    src: 'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
+  },
+  bcat: {
+    name: 'bcat',
+    catClickCounter: 0,
+    src: 'https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d',
+  },
+  cat: {
+    name: 'cat',
+    catClickCounter: 0,
+    src: 'https://api.time.com/wp-content/uploads/2015/02/cats.jpg',
+  },
+};
 
-let catList = document.querySelector('#catList');
+let view = {
+  catListOl: document.querySelector('#catList'),
+  catDetailDiv: document.querySelector('#catDetailDiv'),
+  renderCatList: function (catsnameList) {
+    for (let name of catsnameList) {
+      console.log(name);
+      let catLi = document.createElement('li');
+      catLi.innerHTML = `${name}`;
+      catLi.addEventListener('click', (e) => {
+        controler.handleCatLiClick(name);
+      });
 
-let counters = [0, 0, 0, 0, 0, 0];
-let catNames = ['acat', 'bcat', 'cat', 'dcat', 'ecat', 'white'];
-let catImgSrcs = [
-  'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png',
-  'https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=49ed3252c0b2ffb49cf8b508892e452d',
-  'https://api.time.com/wp-content/uploads/2015/02/cats.jpg',
-  'https://image.cnbcfm.com/api/v1/image/105828578-1554223245858gettyimages-149052633.jpeg?v=1554223281',
-  'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYfUCx4A6ww-bVMwoZudkJmjkmMtssI6ZWlw&usqp=CAU',
-];
-
-for (let i = 0; i < catNames.length; i++) {
-  let catLi = document.createElement('li');
-  catLi.innerText = catNames[i];
-  catLi.addEventListener('click', (e) => {
-    catContainer.innerHTML = '';
-    catContainer.innerHTML += `<h2>${catNames[i]}</h2>`;
+      this.catListOl.appendChild(catLi);
+    }
+  },
+  renderCatDetail: function (cat) {
+    catDetailDiv.innerHTML = `
+      <h2>${cat.name}</h2>
+      <div>${cat.catClickCounter}</div>
+    `;
     let catImg = document.createElement('img');
-    let catClickCounter = document.createElement('div');
-    catImg.src = catImgSrcs[i];
-    catClickCounter.innerText = counters[i];
-
-    catImg.addEventListener('click', (e) => {
-      counters[i]++;
-      catClickCounter.innerText = counters[i];
-    });
-    catContainer.appendChild(catImg);
     catImg.classList = ['CatImg'];
-    catContainer.appendChild(catClickCounter);
-  });
+    catImg.src = cat.src;
+    catImg.addEventListener('click', (e) => {
+      controler.handleCatClick(cat.name);
+    });
 
-  catList.appendChild(catLi);
-}
+    catDetailDiv.appendChild(catImg);
+  },
+};
 
-//1this not easy
-//2code seems complecate,addeventListener in addEventListener in for loop;
-//3i dont foresee i have addEventListener in addEventListener function,but it seems necessary
-//4add more cats seems not difficult bc code use catNames,catImgSrcs,catClickCounter to render page,only add respect data to these variable.
+let controler = {
+  currentCat: {},
+  init: function () {
+    currentCat = model.acat;
+    view.renderCatList(Object.keys(model));
+    view.renderCatDetail(currentCat);
+  },
+  handleCatClick: function (name) {
+    model[name].catClickCounter++;
+    view.renderCatDetail(currentCat);
+  },
+  handleCatLiClick: function (name) {
+    currentCat = model[name];
+    view.renderCatDetail(currentCat);
+  },
+};
+
+controler.init();
